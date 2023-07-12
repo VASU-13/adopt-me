401dfc818ef72c1dc98db5d5fc36863db25a0bfb
vasu <sharma.vasu1997@gmail.com>
2023-07-12 22:59:08 +0530

    react-intro,pure-react,create-element,pure-react-components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+const Pet = (props) => {
+    return React.createElement("div",{},[
+        React.createElement("h1",{},props.animal),
+        React.createElement("h2",{},props.name),
+        React.createElement("h2",{},props.breed),
+    ])
+    
+}
+const App = () => {
+    return React.createElement("div",
+        {},
+        React.createElement("h1",{},
+        "Adopt Me!"),
+        React.createElement(Pet,{
+            animal: "Dog",
+            name: "Doink",
+            breed: "Mixed"
+        }),
+        React.createElement(Pet,{
+            animal: "Dog",
+            name: "Doink",
+            breed: "Mixed"
+        }),
+        React.createElement(Pet,{
+            animal: "Dog",
+            name: "Doink",
+            breed: "Mixed"
+        })
+
+    )
+}
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
