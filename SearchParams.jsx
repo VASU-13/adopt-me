811c037a34c718b5cb58f508c6f4a22afa11dbbe
vasu <sharma.vasu1997@gmail.com>
2023-07-15 01:20:29 +0530

    setState hook

---
+++ SearchParams.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+const ANIMALS = ["Dog", "Cat", "Rabbit"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const Breeds = [];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={Breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option></option>
+            {Breeds.map((breed) => {
+              return (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import Pet from "./Pet";
+import SearchParams from "../SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mixed" />
+      <Pet name="Doink" animal="cat" breed="Mixed" /> */}
     </div>
   );
 };
